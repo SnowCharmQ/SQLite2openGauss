c848bfb31f6082976993ec2be279c80698aa2b1f
SnowCharm <619022098@qq.com>
2022-08-20 14:45:41 +0800

    TODO: update opengauss_thread.py to insert data to the table

--- opengauss_thread.py
+++ opengauss_thread.py
@@ -1,39 +1,26 @@
-import time
 import logging
 import threading
-import psycopg2
+import time
+
+from connection import OpenGaussConnection
 
 
 class OpenGaussThread(threading.Thread):
 
-    def __init__(self, opengauss_properties):
+    def __init__(self, opengauss: OpenGaussConnection, sqls):
         super().__init__()
-        self.conn_opengauss = None
-        self.opengauss_properties = opengauss_properties
+        self.opengauss = opengauss
+        self.sqls = sqls
 
     def run(self) -> None:
-        i = 0
-        ex = None
-        while True:
-            if i == 5:
-                print("Fail to connect to OpenGauss database")
-                raise ex
-            try:
-                self.conn_opengauss = psycopg2.connect(database=self.opengauss_properties['database.name'],
-                                                       user=self.opengauss_properties['database.user'],
-                                                       password=self.opengauss_properties['database.password'],
-                                                       host=self.opengauss_properties['database.host'],
-                                                       port=self.opengauss_properties['database.port'])
-                logging.info(
-                    "Successfully Log In OpenGauss Database %s As %s" % (
-                        self.opengauss_properties['database.name'], self.opengauss_properties['database.user']))
-                break
-            except Exception as e:
-                i += 1
-                if i == 1:
-                    print("Fail to connect to OpenGauss database. Retry 1 time")
-                else:
-                    print("Fail to connect to OpenGauss database. Retry %d times" % i)
-                logging.error(e)
-                ex = e
-                time.sleep(5)
+        conn = None
+        try:
+            conn = self.opengauss.getconn()
+            # print(self.name)
+            # time.sleep(100) # 测试
+            """TODO: 连接执行每一个表的创建和插入操作"""
+        except Exception as e:
+            logging.error(e)
+        finally:
+            if conn is not None:
+                self.opengauss.putconn(conn)

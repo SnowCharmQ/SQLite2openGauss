6b792be46c41c8ed4081f5b0edc484db98d012ed
SnowCharm <619022098@qq.com>
2022-08-22 09:42:52 +0800

    add OpenGauss schema property

--- test.py
+++ test.py
@@ -19,86 +19,74 @@ def main():
 
     args = parser.parse_args()
 
-    opengauss_properties = {
-        'database.name':'postgres',
-        'database.host':'localhost',
-        'database.port':'15432',
-        'database.user':'gaussdb',
-        'database.password':'sustechD1!'
-    }
-    # is_file_update = False
-    # if args.opengauss != '':
-    #     opengauss_file = str(args.opengauss)
-    #     p = Properties(opengauss_file)
-    #     opengauss_properties = p.get_properties()
-    # else:
-    #     opengauss_file = 'opengauss.properties'
-    # if not opengauss_properties.__contains__('database.name') or opengauss_properties['database.name'] == '':
-    #     opengauss_properties['database.name'] = input("Input the database name of OpenGauss:")
-    #     is_file_update = True
-    # if not opengauss_properties.__contains__('database.host') or opengauss_properties['database.host'] == '':
-    #     opengauss_properties['database.host'] = input("Input the host of OpenGauss:")
-    #     is_file_update = True
-    # if not opengauss_properties.__contains__('database.port') or opengauss_properties['database.port'] == '':
-    #     opengauss_properties['database.port'] = input("Input the port of OpenGauss:")
-    #     is_file_update = True
-    # if not opengauss_properties.__contains__('database.user') or opengauss_properties['database.user'] == '':
-    #     opengauss_properties['database.user'] = input("Input the username of OpenGauss:")
-    #     is_file_update = True
-    # if not opengauss_properties.__contains__('database.password') or opengauss_properties['database.password'] == '':
-    #     opengauss_properties['database.password'] = input("Input the user password of OpenGauss:")
-    #     is_file_update = True
-    # if is_file_update:
-    #     save_message = "Save your input in the %s? [y/n]" % opengauss_file
-    #     flag = input(save_message)
-    #     if flag.upper() == 'Y' or flag.upper() == 'YES':
-    #         Properties.write_properties(opengauss_file, opengauss_properties)
+    opengauss_properties = {}
+    is_file_update = False
+    if args.opengauss != '':
+        opengauss_file = str(args.opengauss)
+        p = Properties(opengauss_file)
+        opengauss_properties = p.get_properties()
+    else:
+        opengauss_file = 'opengauss.properties'
+    if not opengauss_properties.__contains__('database.name') or opengauss_properties['database.name'] == '':
+        opengauss_properties['database.name'] = input("Input the database name of OpenGauss:")
+        is_file_update = True
+    if not opengauss_properties.__contains__('database.schema') or opengauss_properties['database.schema'] == '':
+        opengauss_properties['database.schema'] = input("Input the schema name of OpenGauss:")
+        is_file_update = True
+    if not opengauss_properties.__contains__('database.host') or opengauss_properties['database.host'] == '':
+        opengauss_properties['database.host'] = input("Input the host of OpenGauss:")
+        is_file_update = True
+    if not opengauss_properties.__contains__('database.port') or opengauss_properties['database.port'] == '':
+        opengauss_properties['database.port'] = input("Input the port of OpenGauss:")
+        is_file_update = True
+    if not opengauss_properties.__contains__('database.user') or opengauss_properties['database.user'] == '':
+        opengauss_properties['database.user'] = input("Input the username of OpenGauss:")
+        is_file_update = True
+    if not opengauss_properties.__contains__('database.password') or opengauss_properties['database.password'] == '':
+        opengauss_properties['database.password'] = input("Input the user password of OpenGauss:")
+        is_file_update = True
+    if is_file_update:
+        save_message = "Save your input in the %s? [y/n]" % opengauss_file
+        flag = input(save_message)
+        if flag.upper() == 'Y' or flag.upper() == 'YES':
+            Properties.write_properties(opengauss_file, opengauss_properties)
 
-    sqlite_properties = {  'database.filename':'filmdb.sqlite'  }
-    # is_file_update = False
-    # if args.sqlite != '':
-    #     sqlite_file = str(args.sqlite)
-    #     p = Properties(sqlite_file)
-    #     sqlite_properties = p.get_properties()
-    # else:
-    #     sqlite_file = 'sqlite.properties'
-    # if not sqlite_properties.__contains__('database.filename'):
-    #     sqlite_properties['database.filename'] = input("Input the filename of Sqlite3:")
-    #     is_file_update = True
-    # if is_file_update:
-    #     save_message = "Save your input in the %s? [y/n]" % sqlite_file
-    #     flag = input(save_message)
-    #     if flag.upper() == 'Y' or flag.upper() == 'YES':
-    #         Properties.write_properties(sqlite_file, sqlite_properties)
+    sqlite_properties = {}
+    is_file_update = False
+    if args.sqlite != '':
+        sqlite_file = str(args.sqlite)
+        p = Properties(sqlite_file)
+        sqlite_properties = p.get_properties()
+    else:
+        sqlite_file = 'sqlite.properties'
+    if not sqlite_properties.__contains__('database.filename'):
+        sqlite_properties['database.filename'] = input("Input the filename of Sqlite3:")
+        is_file_update = True
+    if is_file_update:
+        save_message = "Save your input in the %s? [y/n]" % sqlite_file
+        flag = input(save_message)
+        if flag.upper() == 'Y' or flag.upper() == 'YES':
+            Properties.write_properties(sqlite_file, sqlite_properties)
 
     opengauss = OpenGaussConnection(opengauss_properties)
     sqlite = SqliteConnection(sqlite_properties)
 
     conn_sqlite = sqlite.getconn()
-    cursor_sqlite = conn_sqlite.cursor()
 
     dbname = opengauss_properties['database.name']
     dbusername = opengauss_properties['database.user']
+    dbschema = opengauss_properties['database.schema']
     conn_opengauss = opengauss.getconn()
     cursor_opengauss = conn_opengauss.cursor()
     try:
-        # cursor_opengauss.execute("create schema %s authorization %s;" % (dbname, dbusername))
+        cursor_opengauss.execute("create schema %s authorization %s;" % (dbname, dbusername))
         cursor_opengauss.execute("grant usage on schema %s to %s;" % (dbname, dbusername))
-        cursor_opengauss.execute("set search_path to %s;" % dbname)
         conn_opengauss.commit()
         cursor_opengauss.close()
         opengauss.putconn(conn_opengauss)
-
     except psycopg2.errors.DuplicateSchema as e:
         logging.error(e)
         print("schema '%s' already exists" % dbname)
-        cursor_opengauss.close()
-        opengauss.putconn(conn_opengauss)
-        conn_opengauss = opengauss.getconn()
-        cursor_opengauss = conn_opengauss.cursor()
-        cursor_opengauss.execute("grant usage on schema %s to %s;" % (dbname, dbusername))
-        cursor_opengauss.execute("set search_path to %s;" % dbname)
-        conn_opengauss.commit()
         cursor_opengauss.close()
         opengauss.putconn(conn_opengauss)
 
@@ -113,31 +101,27 @@ def main():
     ##将每个表的create+insert放在不同线程########################################
     sqls = []
     thread_list = []
-    boo=0
+    boo = 0
     for sql in conn_sqlite.iterdump():
-        if sql.find("CREATE") != -1 and boo==0:
+        if sql.find("CREATE") != -1 and boo == 0:
             sqls.append(sql)
             # print(sql)
-            boo=1
-        elif sql.find("CREATE") != -1 and boo==1:
+            boo = 1
+        elif sql.find("CREATE") != -1 and boo == 1:
             t = OpenGaussThread(opengauss, sqls)
             thread_list.append(t)
             t.start()
-            sqls=[]
+            sqls = []
             sqls.append(sql)
             # print(sql)
         elif sql.find("INSERT") != -1:
             sqls.append(sql)
             # print(sql)
-    t = OpenGaussThread(opengauss, sqls)
+    t = OpenGaussThread(opengauss, sqls, dbschema)
     thread_list.append(t)
     t.start()
     sqls = []
     ###########################################3333
-
-
-
-
 
     # for i in range(5):
     #     print("sss")
@@ -145,8 +129,8 @@ def main():
     #     thread_list.append(t)
     #     t.start()
     for t in thread_list:
-        print("sss")
         t.join()
+        print("sss")
 
 
 if __name__ == '__main__':
